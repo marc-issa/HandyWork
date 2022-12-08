49aa7baaaa5b6bcc02c4c0cb2d8e90865ada98d2
Marc Issa <issamarc15@gmail.com>
2022-12-08 19:18:29 +0200

    Create get top 5 route

--- api/routes/api.php
+++ api/routes/api.php
@@ -4,6 +4,7 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Users\UserController;
 use App\Http\Controllers\OTPController;
+use App\Http\Controllers\RatingController;
 use App\Http\Controllers\User\WorkerController as UserWorkerController;
 
 Route::group(["prefix"=>"v0.1"], function(){
@@ -12,7 +13,8 @@ Route::group(["prefix"=>"v0.1"], function(){
         Route::post("login", [UserController::class, "login"]);
         Route::post("emailVerf", [OTPController::class, "sendOTP"]);
         Route::post("updatePassword", [UserController::class, "updatePassword"]);
-        
-        
     });
+    
+    Route::get("top5", [RatingController::class, "getTop5"]);
+    
 });
