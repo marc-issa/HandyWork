fd5bb0cf5567bafa8f745ccc6bf723d3a90db292
Marc Issa <issamarc15@gmail.com>
2022-12-08 18:30:53 +0200

    Create migration, model and controller for chats table

---
+++ api/database/migrations/2022_12_08_162601_create_chats_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('chats', function (Blueprint $table) {
+            $table->id();
+            $table->string("message");
+            $table->dateTime("read_at");
+            $table->foreign("worker_id")->references("id")->("workers");
+            $table->foreign("user_id")->references("id")->("users");
+            $table->timestamp("sent_at");
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('chats');
+    }
+};
