2fb7f0e0100f7690b351b60584b8f023fb9d9b02
Marc Issa <issamarc15@gmail.com>
2022-12-08 21:32:41 +0200

    Create model and controller for categories table

--- api/routes/api.php
+++ api/routes/api.php
@@ -1,6 +1,6 @@
 <?php
-use App\Http\Controllers\NotificationController;
 use Illuminate\Http\Request;
+use App\Http\Controllers\NotificationController;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Users\UserController;
 use App\Http\Controllers\OTPController;
