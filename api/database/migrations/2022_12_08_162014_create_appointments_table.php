6580ff3f6b0e968a0a611433695d3d345609352b
Marc Issa <issamarc15@gmail.com>
2022-12-08 18:25:25 +0200

    Create migration, model and controller for appointments table

---
+++ api/database/migrations/2022_12_08_162014_create_appointments_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('appointments', function (Blueprint $table) {
+            $table->id();
+            $table->dateTime("date_and_time");
+            $table->string("status");
+            $table->foreign("worker_id")->references("id")->on("workers");
+            $table->foreign("user_id")->references("id")->on("users");
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('appointments');
+    }
+};
