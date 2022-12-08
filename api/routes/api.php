17118e1199f6cf5fac625dc5dc07c2b88af3fe70
Marc Issa <issamarc15@gmail.com>
2022-12-08 18:58:37 +0200

    Create login, signup, otp and forgot password routes

--- api/routes/api.php
+++ api/routes/api.php
@@ -2,3 +2,14 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Users\UserController;
+use App\Http\Controllers\OTPController;
+
+Route::group(["prefix"=>"v0.1"], function(){
+    Route::group(["prefix"=>"user"], function(){
+        Route::post("signup", [UserController::class, "signup"]);
+        Route::post("login", [UserController::class, "login"]);
+        Route::post("emailVerf", [OTPController::class, "sendOTP"]);
+        Route::post("updatePassword", [UserController::class, "updatePassword"]);
+    });
+});
