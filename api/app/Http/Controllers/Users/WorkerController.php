11e039fe847d65c0b1435fdc634e863c00b70e2d
Marc Issa <issamarc15@gmail.com>
2022-12-09 02:06:55 +0200

    Create add worker function

--- api/app/Http/Controllers/Users/WorkerController.php
+++ api/app/Http/Controllers/Users/WorkerController.php
@@ -1,11 +1,26 @@
 <?php
 
-namespace App\Http\Controllers\User;
+namespace App\Http\Controllers\Users;
 
 use App\Http\Controllers\Controller;
+use App\Models\Worker;
 use Illuminate\Http\Request;
 
 class WorkerController extends Controller
 {
-    //
+    function addWorker(Request $request){
+        $worker = new Worker();
+        $worker->user_id = $request->user_id;
+        $worker->hourly_rate = $request->hr;
+
+        if($worker->save()){
+            return response()->json([
+                "resp" => true
+            ]);
+        }else{
+            return response()->json([
+                "resp" => false
+            ]);
+        }
+    }
 }
