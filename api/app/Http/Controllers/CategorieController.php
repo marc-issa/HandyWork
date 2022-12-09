7f060d21c1f2bd9a59944f710898a6a53334fe76
Marc Issa <issamarc15@gmail.com>
2022-12-09 02:57:18 +0200

    Creating add and delete fuction for categorie table

--- api/app/Http/Controllers/CategorieController.php
+++ api/app/Http/Controllers/CategorieController.php
@@ -2,9 +2,37 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Categorie;
 use Illuminate\Http\Request;
 
 class CategorieController extends Controller
 {
-    //
+    function addCategorie(Request $request){
+        $catg = new Categorie();
+        $catg->worker_id = $request->worker_id;
+        $catg->categorie = $request->catg;
+
+        if($catg->save()){
+            return response()->json([
+                "resp"=>true
+            ]);
+        }else{
+            return response()->json([
+                "resp"=>false
+            ]);
+        }
+    }
+
+    function deleteCategorie(Request $request){
+        $catg = Categorie::where("id",$request->catg_id);
+        if($catg->delete()){
+            return response()->json([
+                "resp"=>true
+            ]);
+        }else{
+            return response()->json([
+                "resp"=>false
+            ]);
+        }
+    }
 }
