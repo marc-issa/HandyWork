fedf7eeca7f333b8fd40a08226a8e81dc2e32d2f
Marc Issa <issamarc15@gmail.com>
2022-12-09 03:56:51 +0200

    Create add notification

--- api/app/Http/Controllers/NotificationController.php
+++ api/app/Http/Controllers/NotificationController.php
@@ -2,9 +2,25 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Notification;
 use Illuminate\Http\Request;
 
 class NotificationController extends Controller
 {
-    //
+    function addNotification(Request $request){
+        $notification = new Notification();
+        $notification->user_id = $request->user_id;
+        $notification->content = $request->content;
+        $notification->type = $request->type;
+
+        if($notification->save()){
+            return response()->json([
+                "resp"=>true
+            ]);
+        }else{
+            return response()->json([
+                "resp"=>false
+            ]);
+        }
+    }
 }
