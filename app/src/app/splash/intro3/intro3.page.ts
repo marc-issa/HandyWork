95a86a12f4d56718b2855426f00df301b4e48698
Marc Issa <issamarc15@gmail.com>
2022-12-10 19:37:44 +0200

    Create design and navigation for Intro 3

--- app/src/app/splash/intro3/intro3.page.ts
+++ app/src/app/splash/intro3/intro3.page.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-intro3',
@@ -7,9 +8,13 @@ import { Component, OnInit } from '@angular/core';
 })
 export class Intro3Page implements OnInit {
 
-  constructor() { }
+  constructor(private route: Router) { }
 
   ngOnInit() {
+  }
+
+  navBack() {
+    this.route.navigate(["/intro2"])
   }
 
 }
