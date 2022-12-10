7a58979a738f7f12b7244a266bcbaed79d5b9907
Marc Issa <issamarc15@gmail.com>
2022-12-10 14:35:35 +0200

    Fix app routes

--- app/src/app/app-routing.module.ts
+++ app/src/app/app-routing.module.ts
@@ -4,12 +4,28 @@ import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 const routes: Routes = [
   {
     path: 'home',
-    loadChildren: () => import('./home/home.module').then( m => m.HomePageModule)
+    loadChildren: () => import('./home/home.module').then(m => m.HomePageModule)
   },
   {
     path: '',
-    redirectTo: 'home',
+    redirectTo: 'splash-screen',
     pathMatch: 'full'
+  },
+  {
+    path: 'splash-screen',
+    loadChildren: () => import('./splash/splash-screen/splash-screen.module').then(m => m.SplashScreenPageModule)
+  },
+  {
+    path: 'intro1',
+    loadChildren: () => import('./splash/intro1/intro1.module').then(m => m.Intro1PageModule)
+  },
+  {
+    path: 'intro2',
+    loadChildren: () => import('./splash/intro2/intro2.module').then(m => m.Intro2PageModule)
+  },
+  {
+    path: 'intro3',
+    loadChildren: () => import('./splash/intro3/intro3.module').then(m => m.Intro3PageModule)
   },
 ];
 
