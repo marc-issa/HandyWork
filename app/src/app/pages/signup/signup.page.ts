46c705b9d6d4a1c0eae70f6eee802b12f563e2fe
Marc Issa <issamarc15@gmail.com>
2022-12-10 23:13:26 +0200

    Create home page and Finish Signup page

--- app/src/app/pages/signup/signup.page.ts
+++ app/src/app/pages/signup/signup.page.ts
@@ -22,6 +22,36 @@ export class SignupPage implements OnInit {
   }
 
   signup() {
+    let regex = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
+    if (this.email.match(regex)) {
+      let postData = {
+        "username": this.username,
+        "fname": this.fname,
+        "lname": this.lname,
+        "email": this.email,
+        "address": this.address,
+        "password": this.password
+      }
+      this.http.post("http://127.0.0.1:8000/api/v0.1/user/signup", postData)
+        .subscribe(data => {
+          let tmp_data = JSON.stringify(data);
+          let resp = JSON.parse(tmp_data)["resp"];
+
+          if (resp == "username-exists") {
+            this.resp_err = "Username already exists"
+          } else if (resp == "email-exists") {
+            this.resp_err = "Email already exists"
+          } else {
+            console.log("redirecting")
+          }
+
+        }, error => {
+          console.log(error);
+        });
+    } else {
+      this.resp_err = "Please enter a valid email"
+    }
+
 
   }
 
