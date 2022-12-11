b57e1c9ffdf9b599588c727b54508e32271f8b81
Marc Issa <issamarc15@gmail.com>
2022-12-11 22:56:24 +0200

    Delete all pages for categories and send params with navigation to the categories page so from the params the requests will be handeled

--- app/src/app/app-routing.module.ts
+++ app/src/app/app-routing.module.ts
@@ -36,28 +36,8 @@ const routes: Routes = [
     loadChildren: () => import('./pages/tabs/tabs.module').then(m => m.TabsPageModule)
   },
   {
-    path: 'clean',
-    loadChildren: () => import('./pages/categories/clean/clean.module').then(m => m.CleanPageModule)
-  },
-  {
-    path: 'plumber',
-    loadChildren: () => import('./pages/categories/plumber/plumber.module').then(m => m.PlumberPageModule)
-  },
-  {
-    path: 'painter',
-    loadChildren: () => import('./pages/categories/painter/painter.module').then(m => m.PainterPageModule)
-  },
-  {
-    path: 'electrician',
-    loadChildren: () => import('./pages/categories/electrician/electrician.module').then( m => m.ElectricianPageModule)
-  },
-  {
     path: 'car',
-    loadChildren: () => import('./pages/categories/car/car.module').then( m => m.CarPageModule)
-  },
-  {
-    path: 'washing',
-    loadChildren: () => import('./pages/categories/washing/washing.module').then( m => m.WashingPageModule)
+    loadChildren: () => import('./pages/categories/car/car.module').then(m => m.CarPageModule)
   },
 ];
 
--- app/src/app/pages/categories/car/car.page.ts
+++ app/src/app/pages/categories/car/car.page.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-car',
@@ -6,10 +8,47 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./car.page.scss'],
 })
 export class CarPage implements OnInit {
-
-  constructor() { }
-
+  searchTerm: String = '';
+  workers = [];
+  all_workers = [];
+  constructor(private router: Router, private http: HttpClient) { }
+  categorie: any = '';
   ngOnInit() {
+    let page = this.router.getCurrentNavigation()?.extras.state;
+    let tmp_name = JSON.parse(JSON.stringify(page))["page"];
+    this.categorie = tmp_name;
+
+    let catg = tmp_name.toLowerCase();
+
+    if (this.categorie != "Car Washing") {
+      catg = catg.slice(0, -1);
+    }
+
+    this.http.get(`http://127.0.0.1:8000/api/v0.1/worker/find/getAll/${catg}`)
+      .subscribe(data => {
+        let tmp_data = JSON.stringify(data);
+        this.all_workers = JSON.parse(tmp_data)["resp"];
+      })
   }
 
+  getResult() {
+    let searchTerm = this.searchTerm.toLowerCase();
+    if (searchTerm != "") {
+      this.http.get(`http://127.0.0.1:8000/api/v0.1/worker/find/username/${searchTerm}`)
+        .subscribe(data => {
+          let tmp_data = JSON.stringify(data);
+          this.workers = JSON.parse(tmp_data)["resp"];
+        })
+      if (this.workers.length == 0) {
+        searchTerm = this.searchTerm.charAt(0).toUpperCase() + this.searchTerm.slice(1)
+        this.http.get(`http://127.0.0.1:8000/api/v0.1/worker/find/address/${searchTerm}`)
+          .subscribe(data => {
+            let tmp_data = JSON.stringify(data);
+            this.workers = JSON.parse(tmp_data)["resp"];
+          })
+      }
+    } else {
+      this.workers = [];
+    }
+  }
 }
